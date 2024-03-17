e98279f1f62823bf7297f967802ff8bd2db9b328
Maggie-karuri <karurimargaret111@gmail.com>
2024-03-18 00:00:57 +0300

    move dodge left and right

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const rightEdge = left + dodger.clientWidth;
+  
+    if (rightEdge < 400) { 
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  
