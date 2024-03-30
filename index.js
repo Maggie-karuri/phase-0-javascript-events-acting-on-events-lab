ec601826d59b9073aedea3a61b21ccc387bb46d8
Maggie-karuri <karurimargaret111@gmail.com>
2024-03-31 02:55:04 +0300

    use of event listeners to change the web page

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-function moveDodgerRight() {
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
-    const rightEdge = left + dodger.clientWidth;
   
-    if (rightEdge < 400) { 
-      dodger.style.left = `${left + 1}px`;
-    }
-  }
-  document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowRight") {
-      moveDodgerRight();
-    }
-  });
-  function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
     if (left > 0) {
       dodger.style.left = `${left - 1}px`;
     }
   }
-  document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowLeft") {
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
       moveDodgerLeft();
     }
   });
-  
+
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
